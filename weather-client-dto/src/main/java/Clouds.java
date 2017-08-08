b17b8828eee2e00f00664ef0efaf2f64215ca6a8
Yevhen Vorobiei <yvorobiei@ancestry.com>
2017-08-08 17:37:39 +0300

    init

---
+++ weather-client-dto/src/main/java/Clouds.java
@@ -0,0 +1,15 @@
+/**
+ * @author Yevhen Vorobiei
+ * @since JDK 1.8
+ */
+public class Clouds {
+    private int all;
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
---
+++ weather-client-dto/src/main/java/Coord.java
@@ -0,0 +1,24 @@
+/**
+ * @author Yevhen Vorobiei
+ * @since JDK 1.8
+ */
+public class Coord {
+    private double lon;
+    private double lat;
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+}
